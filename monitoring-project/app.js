c962a6bcd0316b513602937af6011a7d108d73be
javeriya27dev <khanjaveriya0705@gmail.com>
2026-02-18 23:46:37 +0530

    Add files via upload

---
+++ monitoring-project/app.js
@@ -0,0 +1,1 @@
+﻿const express = require("express"); const app = express(); app.get("/", (req, res) => res.send("<h1>Monitoring Project is Live! 📈</h1>")); app.listen(3000, () => console.log("Ready!"));
